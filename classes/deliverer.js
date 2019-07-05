580670c7ad404ca581e73dba63506ae4cc019a2f
zodiacaca <121059464@qq.com>
2019-07-05 15:16:46 +0800

    u

--- classes/deliverer.js
+++ classes/deliverer.js
@@ -1,47 +1,47 @@
 
-const maps = require('./_WeakMap');
-const DomainCodes = new maps.DomainCodes();
+const maps = require('./_WeakMap')
+const DomainCodes = new maps.DomainCodes()
 
 
 const divideMethodString = (method) => {
-  const index = method.indexOf('.');
+  const index = method.indexOf('.')
 
   return {
     domain: method.substring(0, index),
     command: method.substring(index + 1),
-  };
-};
+  }
+}
 
 const getIncrementalId = (domain) => {
-  DomainCodes[domain]++;
+  DomainCodes[domain]++
 
-  return DomainCodes[domain];
-};
+  return DomainCodes[domain]
+}
 
 const stampResult = (rst, stp) => {
-  rst.stamp = stp;
+  rst.stamp = stp
 
-  return rst;
-};
+  return rst
+}
 
 module.exports = {
   Order: class {
     constructor(ws, handler) {
-      this.WebSocket = ws;
-      this.Handler = handler;
-      this.results = [];
-      this.SessionID = undefined;
+      this.WebSocket = ws
+      this.Handler = handler
+      this.results = []
+      this.SessionID = undefined
       // retry relative
-      this.rSuccessCount = 0;
-      this.rHistoryCount = 0;
-      this.rLastOrder = undefined;
-      this.rMaximumNumberOfAttempts = 5;
+      this.rSuccessCount = 0
+      this.rHistoryCount = 0
+      this.rLastOrder = undefined
+      this.rMaximumNumberOfAttempts = 5
     }
 
     static async Send(method, options = null, retry = false) {
-      const domain = divideMethodString(method).domain;
-      const command = divideMethodString(method).command;
-      const id = getIncrementalId(domain);
+      const domain = divideMethodString(method).domain
+      const command = divideMethodString(method).command
+      const id = getIncrementalId(domain)
       const response = await this.Handler.async(
         this.WebSocket,
         {
@@ -50,70 +50,70 @@ module.exports = {
           method: method,
           params: options,
         }
-      );
+      )
 
       return new Promise(resolve => {
         if (!response.code && response.result) {
           const stamp = {
             id: id,
             method: method,
-          };
-          const result = stampResult(response.result, stamp);
-          this.results.push(result);
-          this.rSuccessCount++;
+          }
+          const result = stampResult(response.result, stamp)
+          this.results.push(result)
+          this.rSuccessCount++
         } else {
-          console.log('\x1b[35m', method + ':', '\x1b[0m', response);
+          console.log('\x1b[35m', method + ':', '\x1b[0m', response)
           if (retry) {
-            console.log('\x1b[31m', "Bad! Retry again!", '\x1b[0m');
+            console.log('\x1b[31m', "Bad! Retry again!", '\x1b[0m')
           } else {
-            console.log('\x1b[31m', "Bad!", '\x1b[0m');
+            console.log('\x1b[31m', "Bad!", '\x1b[0m')
           }
         }
         if (!retry) {
-          this.rHistoryCount++;
+          this.rHistoryCount++
         }
-        this.rLastOrder = { m: method, o: options };
+        this.rLastOrder = { m: method, o: options }
 
-        resolve(response);
-      });
+        resolve(response)
+      })
     }
 
     async getLastResult() {
-      let result = this.results[this.rHistoryCount - 1];
+      let result = this.results[this.rHistoryCount - 1]
 
       if (!result) {
         for (let i = 0; i < this.rMaximumNumberOfAttempts; i++) {
-          const response = await this.retryLastOrder();
+          const response = await this.retryLastOrder()
           if (!response.code && response.result) {
-            result = this.results[this.rHistoryCount - 1];
-            break;
+            result = this.results[this.rHistoryCount - 1]
+            break
           } else {
-            console.error(response);
+            console.error(response)
           }
         }
       }
 
       if (result) {
         return new Promise(resolve => {
-          const rst = this.rSuccessCount > 0 ? result : null;
-          resolve(rst);
-        });
+          const rst = this.rSuccessCount > 0 ? result : null
+          resolve(rst)
+        })
       }
     }
 
     printLastResult() {
-      let rst = this.results[this.rHistoryCount - 1];
-      console.log('\x1b[35m', 'Result:', '\x1b[0m', rst);
+      let rst = this.results[this.rHistoryCount - 1]
+      console.log('\x1b[35m', 'Result:', '\x1b[0m', rst)
     }
 
     static retryLastOrder() {
 
       return new Promise(resolve => {
         setTimeout(async () => {
-          const response = await this.Send(this.rLastOrder.m, this.rLastOrder.o, true);
-          resolve(response);
-        }, 500);
-      });
+          const response = await this.Send(this.rLastOrder.m, this.rLastOrder.o, true)
+          resolve(response)
+        }, 500)
+      })
     }
   },
 }
--- launch.js
+++ launch.js
@@ -1,30 +1,29 @@
 
-const fs = require('fs');
+const fs = require('fs')
 
 // const options = {
 //   persistent: true,
 //   recursive: true,
 //   encoding: 'utf8'
-// };
+// }
 // fs.watch('D:/DL/fdm', options, (eventType, filename) => {
-//   console.log(`event type is: ${eventType}`);
+//   console.log(`event type is: ${eventType}`)
 //   if (filename) {
-//     console.log(`filename is: ${filename}`);
+//     console.log(`filename is: ${filename}`)
 //   } else {
-//     console.log('filename not provided');
+//     console.log('filename not provided')
 //   }
-// });
+// })
 
-const readConfig = require('./methods/readConfig');
-const config = readConfig(fs, __dirname + '/launch.json');
+const readConfig = require('./methods/readConfig')
+const config = readConfig(fs, __dirname + '/launch.json')
 
-const WebSocket = require('ws');
-const puppeteer = require('puppeteer');
+const WebSocket = require('ws')
+const puppeteer = require('puppeteer')
 
-const deliverer = require('./classes/deliverer');
+const deliverer = require('./classes/deliverer')
 
-
-(async () => {
+;(async () => {
   const launchOptions = {
     headless: false,
     executablePath: config.browser,
@@ -32,66 +31,67 @@ const deliverer = require('./classes/deliverer');
     args: [
       '--disable-infobars',
       '--start-maximized',
+      '--lang=en-US,en',
     ]
-  };
-  const browser = await puppeteer.launch(launchOptions);
+  }
+  const browser = await puppeteer.launch(launchOptions)
 
   // Create a websocket to issue CDP commands.
-  const ws = new WebSocket(browser.wsEndpoint(), { perMessageDeflate: false });
-  await new Promise(resolve => ws.once('open', resolve));
-  console.log('WebSocket connected!');
+  const ws = new WebSocket(browser.wsEndpoint(), { perMessageDeflate: false })
+  await new Promise(resolve => ws.once('open', resolve))
+  console.log('WebSocket connected!')
 
 
-  const Order = new deliverer.Order(ws, require('./classes/handler'));
+  const Order = new deliverer.Order(ws, require('./classes/handler'))
 
-  await Order.Send('Target.getTargets');
-  const pageInfo = (await Order.getLastResult()).targetInfos.find(info => info.type == 'page');
+  await Order.Send('Target.getTargets')
+  const pageInfo = (await Order.getLastResult()).targetInfos.find(info => info.type == 'page')
 
   await Order.Send('Target.attachToTarget',
     {
       targetId: pageInfo.targetId,
       flatten: true,
     },
-  );
-  Order.SessionID = (await Order.getLastResult()).sessionId;
+  )
+  Order.SessionID = (await Order.getLastResult()).sessionId
 
   await Order.Send('Page.navigate',
     {
       url: 'https://cn.bing.com',
     }
-  );
+  )
 
-  await Order.Send('DOM.enable');
+  await Order.Send('DOM.enable')
 
-  await Order.Send('CSS.enable');
+  await Order.Send('CSS.enable')
 
-  await Order.Send('DOM.getDocument');
-  const rootId = (await Order.getLastResult()).root.nodeId;
+  await Order.Send('DOM.getDocument')
+  const rootId = (await Order.getLastResult()).root.nodeId
 
   await Order.Send('DOM.querySelector',
     {
       nodeId: rootId,
       selector: 'body',
     },
-  );
-  const bodyId = (await Order.getLastResult()).nodeId;
+  )
+  const bodyId = (await Order.getLastResult()).nodeId
 
   await Order.Send('CSS.getComputedStyleForNode',
     {
       nodeId: bodyId,
     },
-  );
-  const styles = (await Order.getLastResult()).computedStyle;
+  )
+  const styles = (await Order.getLastResult()).computedStyle
   // for (let key in styles) {
-  //   console.log(styles[key]);
+  //   console.log(styles[key])
   // }
 
-  await Order.Send('Page.captureScreenshot');
-  const data = (await Order.getLastResult()).data;
+  await Order.Send('Page.captureScreenshot')
+  const data = (await Order.getLastResult()).data
   // fs.writeFile("./tmp/data.txt", data, function(err) {
-  //   err ? console.log(err) : console.log("File saved!");
-  // });
+  //   err ? console.log(err) : console.log("File saved!")
+  // })
   // fs.writeFile("./tmp/out.png", data, 'base64', function(err) {
-  //   err ? console.log(err) : console.log("Image saved!");
-  // });
-})();
+  //   err ? console.log(err) : console.log("Image saved!")
+  // })
+})()
