f841d6d5958baa41d48cd9664625d23fb12e8d25
zodiacaca <121059464@qq.com>
2019-07-18 08:06:27 +0800

    u

--- launch.js
+++ launch.js
@@ -1,27 +1,13 @@
 
 const fs = require('fs')
 
-// const options = {
-//   persistent: true,
-//   recursive: true,
-//   encoding: 'utf8'
-// }
-// fs.watch('D:/DL/fdm', options, (eventType, filename) => {
-//   console.log(`event type is: ${eventType}`)
-//   if (filename) {
-//     console.log(`filename is: ${filename}`)
-//   } else {
-//     console.log('filename not provided')
-//   }
-// })
-
 const readConfig = require('./methods/readConfig')
 const config = readConfig(fs, __dirname + '/launch.json')
 
-const WebSocket = require('ws')
 const puppeteer = require('puppeteer')
 
-const deliverer = require('./classes/deliverer')
+const { URL } = require('url')
+
 
 ;(async () => {
   const launchOptions = {
@@ -35,63 +21,21 @@ const deliverer = require('./classes/deliverer')
     ]
   }
   const browser = await puppeteer.launch(launchOptions)
+  const address = 'http://127.0.0.1:3000'
+  const page = (await browser.pages())[0]
 
-  // Create a websocket to issue CDP commands.
-  const ws = new WebSocket(browser.wsEndpoint(), { perMessageDeflate: false })
-  await new Promise(resolve => ws.once('open', resolve))
-  console.log('WebSocket connected!')
-
-
-  const Order = new deliverer.Order(ws, require('./classes/handler'))
-
-  await Order.Send('Target.getTargets')
-  const pageInfo = (await Order.getLastResult()).targetInfos.find(info => info.type == 'page')
-
-  await Order.Send('Target.attachToTarget',
-    {
-      targetId: pageInfo.targetId,
-      flatten: true,
-    },
-  )
-  Order.SessionID = (await Order.getLastResult()).sessionId
-
-  await Order.Send('Page.navigate',
-    {
-      url: 'https://cn.bing.com',
+  await page.setRequestInterception(true)
+  page.on('request', interceptedRequest => {
+    const url = new URL(interceptedRequest.url())
+    console.log(url.hostname)
+    if (url.hostname == '127.0.0.1') {
+      interceptedRequest.continue()
+    } else {
+      interceptedRequest.abort()
     }
-  )
+  })
 
-  await Order.Send('DOM.enable')
-
-  await Order.Send('CSS.enable')
-
-  await Order.Send('DOM.getDocument')
-  const rootId = (await Order.getLastResult()).root.nodeId
-
-  await Order.Send('DOM.querySelector',
-    {
-      nodeId: rootId,
-      selector: 'body',
-    },
-  )
-  const bodyId = (await Order.getLastResult()).nodeId
-
-  await Order.Send('CSS.getComputedStyleForNode',
-    {
-      nodeId: bodyId,
-    },
-  )
-  const styles = (await Order.getLastResult()).computedStyle
-  // for (let key in styles) {
-  //   console.log(styles[key])
-  // }
-
-  await Order.Send('Page.captureScreenshot')
-  const data = (await Order.getLastResult()).data
-  // fs.writeFile("./tmp/data.txt", data, function(err) {
-  //   err ? console.log(err) : console.log("File saved!")
-  // })
-  // fs.writeFile("./tmp/out.png", data, 'base64', function(err) {
-  //   err ? console.log(err) : console.log("Image saved!")
-  // })
+  await page.goto(address, {
+    // waitUntil: 'load'
+  })
 })()
